924a0ab8d09085fee32c85b17e3053ae85365ed6
Mariam El <mariamchokolade@gmail.com>
2024-03-26 11:31:39 +0200

    list

--- Game.cs
+++ Game.cs
@@ -26,12 +26,25 @@ namespace Genspil
         public class Game
         {
             // Properties of the game
-            public string Title { get; set; }   // Title of the game
+            public string Title { get; set; } // Title of the game
+            public string Edition { get; set; }   // edition of the game
             public decimal BasePrice { get; set; }  // The original price of the game
             public string Genre { get; set; }  // Genre of the game
             public int NumberOfPlayers { get; set; }  // number of players 
             public GameCondition Condition { get; set; } // Condition of the game
             public GameStatus Status { get; set; }  // Status of availiblty in storage 
+
+            //Constructor 
+            public Game(string title, string edition, decimal basePrice, string genre, int numberOfPlayers, GameCondition condition, GameStatus status)
+            {
+                Title = title;
+                Edition = edition;
+                BasePrice = basePrice;
+                Genre = genre;
+                NumberOfPlayers = numberOfPlayers;
+                Condition = condition;
+                Status = status;
+            }
 
             // Calculating the price after condition
             public decimal CalculatePrice()
@@ -56,34 +69,14 @@ namespace Genspil
 
         }
 
-        // A list including All the games in storage
-        public static List<Game> GetGamesList()
-        {
-            return new List<Game>
-            {
-            // Sequence
-            new Game { Title = Sequence , Genre =  , BasePrice = 150m , Condition = GameCondition.Good, 2},
-            new Game { Title = , Genre = , BasePrice = , Condition = GameCondition. },
-            new Game { Title = , Genre = , BasePrice = , Condition = GameCondition. },
-            new Game { Title = , Genre = , BasePrice = , Condition = GameCondition. },
-            new Game { Title = , Genre = , BasePrice = , Condition = GameCondition. },
-            new Game { Title = , Genre = , BasePrice = , Condition = GameCondition. },
-            new Game { Title = , Genre = , BasePrice = , Condition = GameCondition. },
-
-            }
-        }
-     
-    
-
-        public static void AddGame(List<Game> gamesList, string title, string genre, decimal price, GameCondition condition, int count, int numberofplayers)
-        {
-            for (int i = 0; i < count; i++)
-            {
-                gamesList.Add(new Game { Title = title, Genre = genre, BasePrice = price, Condition = condition, NumberOfPlayers = numberofplayers })
-            }
-        }
-
-
     }
+    // Array to store game information
+    private static Gameinfo[] gamesarray = new Gameinfo[100]; //Assuming a maximum of 100 games
+    private static int nextIndex = 0; // Keep track of the next available index in the array
+    
+    // Method to add a game directly into the array
+    public static void AddGame (string )
+
+    
 }
 
