6ad43df0926f36ee0fe96e380e8cf2d336544c29
Mariam El <mariamchokolade@gmail.com>
2024-04-02 09:43:17 +0200

    Gamedata

--- Game.cs
+++ Game.cs
@@ -70,13 +70,6 @@ namespace Genspil
         }
 
     }
-    // Array to store game information
-    private static Gameinfo[] gamesarray = new Gameinfo[100]; //Assuming a maximum of 100 games
-    private static int nextIndex = 0; // Keep track of the next available index in the array
-    
-    // Method to add a game directly into the array
-    public static void AddGame (string )
-
     
 }
 
